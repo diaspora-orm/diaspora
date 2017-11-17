49299ae2a19abe713818a5734eeadd65aba0a465
Gerkin <nihilivin@gmail.com>
2017-11-17 17:39:32 +0100

    Basic relations implementation

--- test/models/index.js
+++ test/models/index.js
@@ -6,3 +6,4 @@ importTest( getStyle( 'model', 'Test entities & sets' ), `${ __dirname  }/compon
 importTest( getStyle( 'model', 'Simple model (single source)' ), `${ __dirname  }/simple.js` );
 importTest( getStyle( 'model', 'Simple model with remapping (single source)' ), `${ __dirname  }/simple-remapping.js` );
 importTest( getStyle( 'model', 'Simple model with validations (single source)' ), `${ __dirname  }/validations.js` );
+importTest( getStyle( 'model', 'Relations (single source)' ), `${ __dirname  }/relations.js` );
