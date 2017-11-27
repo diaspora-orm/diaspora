cc4d3847369a9c0d5de5f9df37a70a235be3369f
Gerkin <nihilivin@gmail.com>
2017-11-27 18:48:57 +0100

    F*CK U WINDOWS

--- diaspora.js
+++ diaspora.js
@@ -3,7 +3,7 @@
 if ( !process.browser ) {
 	const _ = require( 'lodash' );
 	const cachedDiaspora = _.find( require.cache, ( module, path ) => {
-		return path.endsWith( '/diaspora.js' );
+		return path.endsWith( `${require('path').sep}diaspora.js` );
 	});
 	if ( !_.isEmpty( _.get( cachedDiaspora, 'exports' ))) {
 		console.log( 'Retrieving loaded diaspora' );
