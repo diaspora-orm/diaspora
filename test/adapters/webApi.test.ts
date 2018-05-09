86ed96457b9e653c1199a22682f145fa327b082f
Gerkin <nihilivin@gmail.com>
2018-05-09 02:01:57 +0200

    test(WebApiAdapter): Fix use body-parser modules

--- test/adapters/webApi.test.ts
+++ test/adapters/webApi.test.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import express from 'express';
+import {json, urlencoded} from 'body-parser';
 
 import { Diaspora } from '../../src/diaspora';
 import { IWebApiAdapterConfig } from '../../src/adapters/webApi/adapter';
@@ -30,9 +31,12 @@ beforeAll( () => {
 		'foobar'
 	);
 	const INMEMORY_TABLE = 'test-expressstore';
-	app.use( ENDPOINT, require( 'body-parser' )() );
-	app.use( `${ENDPOINT}s`, require( 'body-parser' )() );
-
+	app.use( urlencoded( {
+		extended: true,
+	} ) );
+	
+	app.use( json() );
+	
 	app.post( ENDPOINT, ( req, res ) => {
 		const body = req.body;
 		inMemoryAdapter.insertOne( INMEMORY_TABLE, body ).then( entity => {
@@ -59,88 +63,88 @@ beforeAll( () => {
 			return res.json();
 		} );
 	} );
-
+	
 	app.get( ENDPOINT, ( req, res ) => {
 		const query = parseQs( req.query );
 		inMemoryAdapter
-			.findOne( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
-			.then( entity => {
-				if ( !_.isNil( entity ) ) {
-					entity.attributes.id = entity.attributes.idHash.foobar;
-					delete entity.attributes.idHash;
-					return res.json( entity.attributes );
-				}
-				return res.json();
-			} );
+		.findOne( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
+		.then( entity => {
+			if ( !_.isNil( entity ) ) {
+				entity.attributes.id = entity.attributes.idHash.foobar;
+				delete entity.attributes.idHash;
+				return res.json( entity.attributes );
+			}
+			return res.json();
+		} );
 	} );
 	app.get( `${ENDPOINT}s`, ( req, res ) => {
 		const query = parseQs( req.query );
 		inMemoryAdapter
-			.findMany( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
-			.then( entities => {
-				if ( !_.isEmpty( entities ) ) {
-					return res.json(
-						_.map( entities, ( entity: AdapterEntity ) => {
-							entity.attributes.id = entity.attributes.idHash.foobar;
-							delete entity.attributes.idHash;
-							return entity.attributes;
-						} )
-					);
-				}
-				return res.json( [] );
-			} );
+		.findMany( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
+		.then( entities => {
+			if ( !_.isEmpty( entities ) ) {
+				return res.json(
+					_.map( entities, ( entity: AdapterEntity ) => {
+						entity.attributes.id = entity.attributes.idHash.foobar;
+						delete entity.attributes.idHash;
+						return entity.attributes;
+					} )
+				);
+			}
+			return res.json( [] );
+		} );
 	} );
-
+	
 	app.patch( ENDPOINT, ( req, res ) => {
 		const body = req.body;
 		const query = parseQs( req.query );
 		inMemoryAdapter
-			.updateOne( INMEMORY_TABLE, query.where, body, _.omit( query, ['where'] ) )
-			.then( entity => {
-				if ( !_.isNil( entity ) ) {
-					entity.attributes.id = entity.attributes.idHash.foobar;
-					delete entity.attributes.idHash;
-					return res.json( entity.attributes );
-				}
-				return res.json( entity );
-			} );
+		.updateOne( INMEMORY_TABLE, query.where, body, _.omit( query, ['where'] ) )
+		.then( entity => {
+			if ( !_.isNil( entity ) ) {
+				entity.attributes.id = entity.attributes.idHash.foobar;
+				delete entity.attributes.idHash;
+				return res.json( entity.attributes );
+			}
+			return res.json( entity );
+		} );
 	} );
 	app.patch( `${ENDPOINT}s`, ( req, res ) => {
 		const body = req.body;
 		const query = parseQs( req.query );
 		inMemoryAdapter
-			.updateMany( INMEMORY_TABLE, query.where, body, _.omit( query, ['where'] ) )
-			.then( entities => {
-				if ( !_.isEmpty( entities ) ) {
-					return res.json(
-						_.map( entities, ( entity: AdapterEntity ) => {
-							entity.attributes.id = entity.attributes.idHash.foobar;
-							delete entity.attributes.idHash;
-							return entity.attributes;
-						} )
-					);
-				}
-				return res.json( [] );
-			} );
+		.updateMany( INMEMORY_TABLE, query.where, body, _.omit( query, ['where'] ) )
+		.then( entities => {
+			if ( !_.isEmpty( entities ) ) {
+				return res.json(
+					_.map( entities, ( entity: AdapterEntity ) => {
+						entity.attributes.id = entity.attributes.idHash.foobar;
+						delete entity.attributes.idHash;
+						return entity.attributes;
+					} )
+				);
+			}
+			return res.json( [] );
+		} );
 	} );
-
+	
 	app.delete( ENDPOINT, ( req, res ) => {
 		const query = parseQs( req.query );
 		inMemoryAdapter
-			.deleteOne( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
-			.then( () => {
-				return res.json();
-			} );
+		.deleteOne( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
+		.then( () => {
+			return res.json();
+		} );
 	} );
 	app.delete( `${ENDPOINT}s`, ( req, res ) => {
 		const query = parseQs( req.query );
 		inMemoryAdapter
-			.deleteMany( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
-			.then( () => {
-				return res.json();
-			} );
+		.deleteMany( INMEMORY_TABLE, query.where, _.omit( query, ['where'] ) )
+		.then( () => {
+			return res.json();
+		} );
 	} );
-
+	
 	return new Promise( ( resolve, reject ) => {
 		server = app.listen( adapterConfig.port, () => {
 			console.log( `Example app listening on port ${adapterConfig.port}!` );
