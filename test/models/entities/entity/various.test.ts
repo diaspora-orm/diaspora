13d4b6b05729fc3098f21e6a8f3fdff548e0b9dc
Gerkin <nihilivin@gmail.com>
2018-07-03 00:08:57 +0200

    test(Entity): Added tests for replaceAttributes

--- test/models/entities/entity/various.test.ts
+++ test/models/entities/entity/various.test.ts
@@ -10,16 +10,42 @@ it( 'Diff on entity', async () => {
 	};
 	const testedEntity = await model.insert( object );
 	expect( testedEntity ).toBeAnEntity( model, object, SOURCE );
-
+	
 	testedEntity.attributes.foo = 'baz';
 	expect( testedEntity.getDiff() ).toEqual( {foo: 'baz'} );
-
+	
 	testedEntity.attributes.qux = 1;
 	expect( testedEntity.getDiff() ).toEqual( {foo: 'baz', qux: 1} );
-
+	
 	testedEntity.attributes.foo = undefined;
 	expect( testedEntity.getDiff() ).toEqual( {foo: undefined, qux: 1} );
-
+	
 	testedEntity.attributes.qux = undefined;
 	expect( testedEntity.getDiff() ).toEqual( {foo: undefined} );
+} );
+describe( 'Replace entity', () => {
+	it( 'from spawn', async () => {
+		const entity = model.spawn( {foo:1} );
+		expect( entity ).toBeAnEntity( model, {foo:1}, true );
+		entity.replaceAttributes( {bar:'qux'} );
+		expect( entity.attributes ).toEqual( {bar:'qux'} );
+		const insertedEntity = await entity.persist();
+		expect( insertedEntity.attributes ).toEqual( {bar:'qux'} );
+		insertedEntity.replaceAttributes( {baz:'quux'} );
+		const updatedEntity = await insertedEntity.persist();
+		expect( updatedEntity.attributes ).toEqual( {baz:'quux'} );
+	} );
+	it( 'from retrieving', async () => {
+		await model.insert( {foo:'123'} );
+		
+		const retrievedEntity = await model.find( {foo:'123'} );
+		expect( retrievedEntity ).toBeAnEntity( model, {foo:'123'}, false );
+		retrievedEntity.replaceAttributes( {bar:'qux'} );
+		expect( retrievedEntity.attributes ).toEqual( {bar:'qux'} );
+		const replacedEntity = await retrievedEntity.persist();
+		expect( replacedEntity.attributes ).toEqual( {bar:'qux'} );
+		replacedEntity.replaceAttributes( {baz:'quux'} );
+		const updatedEntity = await replacedEntity.persist();
+		expect( updatedEntity.attributes ).toEqual( {baz:'quux'} );
+	} );
 } );
