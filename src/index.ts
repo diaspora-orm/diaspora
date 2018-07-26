c00ffe3ce2344eb14c5a5c638a29e8f18cf72897
Gerkin <nihilivin@gmail.com>
2018-07-26 22:45:33 +0200

    refactor: Expose utils in root namespace `Utils` with generateUUID & getDefaultFunction

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-export { Diaspora, DiasporaStatic } from './diaspora';
+export * from './diaspora';
 
-export { Entities } from './entities';
-export { Errors } from './errors';
-export { QueryLanguage } from './types/queryLanguage';
+export * from './entities';
+export * from './errors';
+export * from './types/queryLanguage';
 
-export { CheckTransformer, DefaultTransformer, EntityTransformer } from './entityTransformers';
-export { Model } from './model';
+export * from './entityTransformers';
+export * from './model';
+
+import { generateUUID as _generateUUID, getDefaultFunction as _getDefaultFunction } from './utils';
+export namespace Utils{
+	export const generateUUID = _generateUUID;
+	
+	export const getDefaultFunction = _getDefaultFunction;
+}
