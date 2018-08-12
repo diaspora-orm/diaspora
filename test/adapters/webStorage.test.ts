c9109d385c8eae198a958747eeefc56c6aaa1285
Gerkin <nihilivin@gmail.com>
2018-08-12 17:20:16 +0200

    test(Adapters.Webstorage): Fixed WebStorage tests

--- test/adapters/webStorage.test.ts
+++ test/adapters/webStorage.test.ts
@@ -1,15 +1,27 @@
-import {clone} from 'lodash';
-import { getConfig } from '../utils';
-// tslint:disable-next-line:no-implicit-dependencies
-import { LocalStorage } from 'node-localstorage';
+class LocalStorageMock{
+	private store = {};
+	public setItem( key, val ){
+		this.store[key] = val;
+	}
+	public getItem( key ){
+		return this.store[key];
+	}
+	public removeItem( key ){ 
+		delete this.store[key];
+	}
+	public clear(){
+		this.store = {};
+	}
+  }
 
-( global as any ).localStorage = clone( new LocalStorage( './.localstorage' ) );
-( global as any ).sessionStorage = clone( new LocalStorage( './.sessionstorage' ) );
+Object.defineProperty( global, 'localStorage', {
+	value: new LocalStorageMock(),
+} );
+Object.defineProperty( global, 'sessionStorage', {
+	value: new LocalStorageMock(),
+} );
 
 const ADAPTER_LABEL = 'webStorage';
-
-localStorage.clear();
-sessionStorage.clear();
 
 import {
 	createDataSource,
@@ -25,35 +37,35 @@ checkSpawnedAdapter( ADAPTER_LABEL );
 checkEachStandardMethods( ADAPTER_LABEL );
 checkApplications( ADAPTER_LABEL );
 describe( `${ADAPTER_LABEL} > Configuration`, () => {
-	it( 'Use local storage', async () => {
+	beforeEach( () => {
 		delete ( Diaspora as any )._dataSources[ADAPTER_LABEL];
-		const mockFn = jest.fn();
-		const oldFn = ( global as any ).localStorage.setItem;
+	} );
+	it( 'Use local storage', async () => {
+		const mockFn = jest.fn( localStorage.setItem );
 		( global as any ).localStorage.setItem = mockFn;
 		const adapter = await Diaspora.createNamedDataSource( ADAPTER_LABEL, ADAPTER_LABEL, {
 			session: false,
 		} ).waitReady();
 		await adapter.insertOne( 'TEST', {} );
 		expect( mockFn ).toHaveBeenCalled();
-		( global as any ).localStorage.setItem = oldFn;
 	} );
 	it( 'Use session storage', async () => {
-		delete ( Diaspora as any )._dataSources[ADAPTER_LABEL];
-		const mockFn = jest.fn();
-		const oldFn = ( global as any ).sessionStorage.setItem;
+		const mockFn = jest.fn( sessionStorage.setItem );
 		( global as any ).sessionStorage.setItem = mockFn;
 		const adapter = await Diaspora.createNamedDataSource( ADAPTER_LABEL, ADAPTER_LABEL, {
 			session: true,
 		} ).waitReady();
 		await adapter.insertOne( 'TEST', {} );
 		expect( mockFn ).toHaveBeenCalled();
-		( global as any ).sessionStorage.setItem = oldFn;
 	} );
 } );
 describe( `${ADAPTER_LABEL} > Error recovery`, () => {
+	beforeEach( () => { 
+		localStorage.clear();
+	} );
 	it( 'Missing indexed items', async () => {
 		delete ( Diaspora as any )._dataSources[ADAPTER_LABEL];
-
+		
 		const adapter = await Diaspora.createNamedDataSource( ADAPTER_LABEL, ADAPTER_LABEL ).waitReady();
 		await adapter.insertMany( 'TEST', [{data: 42}, {data: 21}] );
 		const indexTable = localStorage.getItem( 'TEST' );
