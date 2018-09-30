c717919010446db103997e1065e64575678f8759
Gerkin <nihilivin@gmail.com>
2018-09-30 18:29:56 +0200

    build(rollup): Changed rollup builds for IIFE & ESM on browser, & ESM only for node
    Related to the work 7 researches done for pstephan1187/vue-datatable#47

---
+++ rollup.config.js
@@ -0,0 +1,107 @@
+import jscc from 'rollup-plugin-jscc';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+// TODO: Update `rollup-plugin-terser`
+// See issue https://github.com/TrySound/rollup-plugin-terser/issues/5 that breaks mutli-output
+import { terser } from 'rollup-plugin-terser';
+import babel from 'rollup-plugin-babel';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+
+import { kebabCase } from 'lodash';
+
+// The module name
+const moduleName = 'diaspora';
+const fileName = kebabCase(moduleName);
+
+const env = process.env.ENVIRONMENT || 'development'; // Production or development
+const pkg = require('./package.json');
+
+console.log(`Building ${moduleName} for ${env}.`);
+
+// Plugins used for build
+const getPlugins = browser => [
+	// Preprocess files
+	jscc( {
+		values: {
+			_BROWSER: browser,
+			_ENVIRONMENT: env,
+		},
+		extensions: ['.js', '.ts'],
+	} ),
+	
+	// Compile TypeScript files
+	typescript( {
+		useTsconfigDeclarationDir: true,
+		clean: true,
+		check: true,
+	} ),
+	
+	json(),
+	
+	// Allow node_modules resolution, so you can use 'external' to control
+	// which external modules to include in the bundle
+	// https://github.com/rollup/rollup-plugin-node-resolve#usage
+	resolve( {
+		browser,
+	} ),
+	// Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+	commonjs( {
+		namedExports: {
+			'node_modules/lodash/lodash.js': Object.keys( require( 'lodash' ) ),
+		},
+	} ),
+	
+	env === 'production' ? terser() : undefined,
+	// Resolve source maps to the original source
+	
+	// Downgrade ES
+	babel(),
+	
+	sourceMaps(),
+].filter(v => v);
+
+// Destination dir
+const outDir = 'dist';
+
+// Should we generate source maps?
+const sourcemap = true;
+
+export default [
+	// Browser
+	{
+		input: './src/index.ts',
+		output: {
+			file: `${outDir}/browser/${fileName}.iife.js`,
+			format: 'iife',
+			// Use `name` as window to hack a bit & avoid exports.
+			name: moduleName,
+			sourcemap,
+		},
+		plugins: getPlugins(true),
+	},
+	{
+		input: './src/index.ts',
+		output: {
+			file: `${outDir}/browser/${fileName}.esm.js`,
+			format: 'esm',
+			name: moduleName,
+			sourcemap
+		},
+		plugins: getPlugins(true),
+		external: Object.keys(pkg.dependencies),
+	},
+	// Node
+	{
+		input: './src/index.ts',
+		output: {
+			file: `${outDir}/node/${fileName}.esm.js`,
+			format: 'esm',
+			name: moduleName,
+			sourcemap
+		},
+		plugins: getPlugins(false),
+		external: Object.keys(pkg.dependencies),
+	},
+]
